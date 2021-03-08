3b11d35827194f4d85fd85b9512f1128ee2264c1
maoz lev <maoz20011111@gmail.com>
2021-03-08 14:14:07 +0200

    update

--- Test.cpp
+++ Test.cpp
@@ -10,38 +10,51 @@
 using namespace ariel;
 using namespace std;
 
-TEST_CASE("Good snowman code") {
-    CHECK(snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-    CHECK(snowman(12222212) == string("  _===_\n\\(o.o)/\n  ( : )\n  (" ")"));
-    CHECK(snowman(33232124) == string("    _\n   /_\\\n\\(o_O)\n  (] [)>\n  (   )"));
-    CHECK(snowman(11111111) == string(" _===_\n (.,.)\n<( : )>\n ( : )"));
-    CHECK(snowman(22222222) == string("   ___\n  .....\n\\(o.o)/\n  (] [)\n  (   )\n   " ""));
-    CHECK(snowman(33333333) == string("   _\n  /_\\n (O_O)\n/(> <)\\n (___)"));
-    CHECK(snowman(44444444) == string("  ___\n(_*_)\n(- -)\n(   )\n(   )"));
-    CHECK(snowman(44134444) == string(" ___\n(_*_)\n(. O)\n(   )\n(   )"));
-    CHECK(snowman(24134442) == string("  ___\n .....\n (. O)\n (   )\n (   )\n  " ""));
-    CHECK(snowman(12341234) == string(" _===_\n (O.-)/\n<(> <)\n (   )"));
-    CHECK(snowman(43214321) == string("  ___\n(_*_)\n(o_.)\n(] [)\\n( : )"));
-    CHECK(snowman(12344321) == string("_===_\n(O.-)\n(] [)\\n( : )"));
-    CHECK(snowman(43211234) == string("  ___\n (_*_)\n (o_.)/\n<(> <)\n (   )"));
-    CHECK(snowman(11223344) == string(" _===_\n (o,o)\n/(   )\\n (   )"));
-    CHECK(snowman(11122233) == string("  _===_\n\\(.,o)/\n (> <)\n (___)"));
-    CHECK(snowman(11133344) == string("  _===_\n (.,O)\n/(   )\\n (   )"));
-    CHECK(snowman(44433322) == string("  ___\n (_*_)\n (- O)\n/(] [)\\n (   )\n  " ""));
-    CHECK(snowman(22443311) == string("  ___\n .....\n (-.-)\n/( : )\\n ( : )"));
-    CHECK(snowman(12123434) == string(" _===_\n (..o)\n/(> <)\n (   )"));
-    CHECK(snowman(43432121) == string("   ___\n (_*_)\n\\(-_O)\n (] [)>\n ( : )"));
-    CHECK(snowman(33214422) == string("   _\n /_\\n(o_.)\n(] [)\n(   )\n " ""));
-    CHECK(snowman(32321414) == string("   _\n  /_\\n (O.o)\n<( : )\n (   )"));
-    CHECK(snowman(41144114) == string(" ___\n(_*_)\n(.,-)\n( : )>\n(   )"));
-    CHECK(snowman(12322312) == string(" _===_\n\\(O.o)\n ( : )\\n (   )\n  " ""));
-    CHECK(snowman(44212314) == string("  ___\n (_*_)\n\\(o .)\n ( : )\\n (   )"));
-    CHECK(snowman(31234311) == string("   _\n /_\\n(o,O)\n( : )\\n( : )"));
-    CHECK(snowman(22111122) == string("   ___\n .....\n (...)\n<(] [)>\n (   )\n  " ""));
-    CHECK(snowman(44222244) == string("  ___\n (_*_)\n\\(o o)/\n (   )\n (   )"));
-    CHECK(snowman(11444444) == string("_===_\n(-,-)\n(   )\n(   )"));
-    CHECK(snowman(44223311) == string("   ___\n (_*_)\n (o o)\n/( : )\\n ( : )"));
-}
+TEST_CASE("Good snowman code") { // every snowman have 4 levels:
+//
+// hat, head, belly, base
+// each level have 7 chars
+// ** the hat have 2 levels - because there are hats that have 2 levels.. 
+
+    CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "));
+    CHECK(snowman(11112211) == string("       \n _===_ \n\\(.,.)/\n ( : ) \n ( : ) "));
+    CHECK(snowman(12222212) == string("       \n _===_ \n\\(o.o)/\n ( : ) \n (\" \") "));
+    CHECK(snowman(11112311) == string("       \n _===_ \n\\(.,.) \n ( : )\\\n ( : ) "));
+    CHECK(snowman(13111112) == string("       \n _===_ \n (._.) \n<( : )>\n (\" \") "));
+    CHECK(snowman(12344434) == string("       \n _===_ \n (O.-) \n (> <) \n (   ) "));   
+    CHECK(snowman(14321143) == string("       \n _===_ \n (O o) \n<(   )>\n (___) "));
+    CHECK(snowman(12341234) == string("       \n _===_ \n (O.-)/\n<(> <) \n (   ) "));
+
+    CHECK(snowman(22222222) == string("  ___  \n ..... \n\\(o.o)/\n (] [) \n (\" \") "));
+    CHECK(snowman(21441112) == string("  ___  \n ..... \n (-,-) \n<( : )>\n (\" \") "));
+    CHECK(snowman(22244421) == string("  ___  \n ..... \n (o.-) \n (] [) \n ( : ) "));
+    CHECK(snowman(23113334) == string("  ___  \n ..... \n (._.) \n/(> <)\\\n (   ) "));
+    CHECK(snowman(24333343) == string("  ___  \n ..... \n (O O) \n/(   )\\\n (___) "));
+    CHECK(snowman(22443311) == string("  ___  \n ..... \n (-.-) \n/( : )\\\n ( : ) "));
+    CHECK(snowman(22111122) == string("  ___  \n ..... \n (...) \n<(] [)>\n (\" \") "));
+    CHECK(snowman(24444444) == string("  ___  \n ..... \n (- -) \n (   ) \n (   ) "));
+
+    CHECK(snowman(33333333) == string("   _   \n  /_\\  \n (O_O) \n/(> <)\\\n (___) "));
+    CHECK(snowman(33232124) == string("   _   \n  /_\\  \n\\(o_O) \n (] [)>\n (   ) "));
+    CHECK(snowman(33214422) == string("   _   \n  /_\\  \n (o_.) \n (] [) \n (\" \") "));
+    CHECK(snowman(32321414) == string("   _   \n  /_\\  \n (O.o) \n<( : ) \n (   ) "));
+    CHECK(snowman(31342243) == string("   _   \n  /_\\  \n\\(O,-)/\n (   ) \n (___) "));
+    CHECK(snowman(33221132) == string("   _   \n  /_\\  \n (o_o) \n<(> <)>\n (\" \") "));
+    CHECK(snowman(33442221) == string("   _   \n  /_\\  \n\\(-_-)/\n (] [) \n ( : ) "));
+    CHECK(snowman(34414414) == string("   _   \n  /_\\  \n (- .) \n ( : ) \n (   ) "));
+
+    CHECK(snowman(44444444) == string("  ___  \n (_*_) \n (- -) \n (   ) \n (   ) "));
+    CHECK(snowman(41442221) == string("  ___  \n (_*_) \n\\(-,-)/\n (] [) \n ( : ) "));
+    CHECK(snowman(42224432) == string("  ___  \n (_*_) \n (o.o) \n (> <) \n (\" \") "));
+    CHECK(snowman(43332313) == string("  ___  \n (_*_) \n\\(O_O) \n ( : )\\\n (___) "));
+    CHECK(snowman(44441144) == string("  ___  \n (_*_) \n (- -) \n<(   )>\n (   ) "));
+    CHECK(snowman(43432121) == string("  ___  \n (_*_) \n\\(-_O) \n (] [)>\n ( : ) "));
+    CHECK(snowman(44433322) == string("  ___  \n (_*_) \n (- O) \n/(] [)\\\n (\" \") "));
+    CHECK(snowman(44134444) == string("  ___  \n (_*_) \n (. O) \n (   ) \n (   ) "));
+
+    }
+
+    // checks random short numbers
 
 TEST_CASE("Bad snowman code (short number)") { 
       for (int i = 0; i < 10; i++)
@@ -81,6 +94,8 @@ TEST_CASE("Bad snowman code (short number)") {
    }
 }
 
+// checks random negative short numbers
+
 TEST_CASE("Bad snowman code (negative short number)") { 
       for (int i = 0; i < 10; i++)
    {
@@ -119,6 +134,8 @@ TEST_CASE("Bad snowman code (negative short number)") {
    }
 }
 
+// checks random long numbers
+
 TEST_CASE("Bad snowman code (long number)") {
    for (int i = 0; i < 10; i++)
    {
@@ -126,6 +143,8 @@ TEST_CASE("Bad snowman code (long number)") {
        CHECK_THROWS(snowman(v));
    }
 }
+
+// checks random negative long numbers
 
 TEST_CASE("Bad snowman code (negative long number)") {
    for (int i = 0; i < 10; i++)
@@ -135,8 +154,9 @@ TEST_CASE("Bad snowman code (negative long number)") {
    }
 }
 
-TEST_CASE("Bad snowman code negative number"){
-   
+// checks random negative numbers
+
+TEST_CASE("Bad snowman code negative number"){ 
    for (int i = 0; i < 10; i++)
    {
        int v = rand() % -10000000 + -99999999;
@@ -144,11 +164,13 @@ TEST_CASE("Bad snowman code negative number"){
    }
 }
 
+// checks random invalid number each place
+
 TEST_CASE("Bad snowman code (invalid number every iteration put number between 5-9)"){
     int number = 11111111;
     int j = 0; 
     while(j<20){
-        int v = rand() % 6 + 4;
+        int v = rand() % 6 + 4;//
         for (int i = 0; i < 8; i++)
         {
             //  cout<<v<<endl;
@@ -157,14 +179,6 @@ TEST_CASE("Bad snowman code (invalid number every iteration put number between 5
         }
         j++;
     }
-    CHECK_THROWS(snowman(11111110));
-    CHECK_THROWS(snowman(11111101));
-    CHECK_THROWS(snowman(11111011));
-    CHECK_THROWS(snowman(11110111));
-    CHECK_THROWS(snowman(11101111));
-    CHECK_THROWS(snowman(11011111));
-    CHECK_THROWS(snowman(10111111));
-    CHECK_THROWS(snowman(01111111));
 }
 
 
--- snowman.cpp
+++ snowman.cpp
@@ -6,28 +6,90 @@ using namespace std;
 namespace ariel{
 
 string snowman(int x){
-    if(x<0) throw invalid_argument("not good - negative number...\n");
-    // if(x>99999999) throw invalid_argument("not good - too long number...\n");
-    // count digits
+    if(x<0)
+        throw invalid_argument("not good - negative number...\n");
+    if(x == 0)
+        throw invalid_argument("not good - invalid input...\n");
+    if(x>99999999)
+        throw invalid_argument("not good - too long number...\n");
+    if(x>44444444)
+        throw invalid_argument("not good - invalid input...\n");
+    
+    int list[8]; // my list to build a snowman!
+        
+    string hat[4] = { "       \n _===_ \n", "  ___  \n ..... \n","   _   \n  /_\\  \n","  ___  \n (_*_) \n" }; //0
+    string nose[4] = { ",", ".", "_", " " }; // 1
+    string left_eye[4] = { "(.", "(o", "(O", "(-" }; // 1
+    string right_eye[4] = { ".)", "o)", "O)", "-)" }; // 1
+    string left_arm[4] = { "<", "\\", "/", " " }; // if \ 1 else 2
+    string right_arm[4] = { ">", "/", "\\", " " }; // if / 1 else 2
+    string torso[4] = { "( : )", "(] [)", "(> <)", "(   )" }; // 2
+    string base[4] = { " ( : ) ", " (\" \") ", " (___) " , " (   ) " }; // 3
+
+     // count digits and find out if there are any invalid digits
     int tmp = x;
     int count = 0;
+    int index_list = 7; 
+
     while(tmp != 0){
         if(tmp%10>4)
-            throw invalid_argument("not good - number between 5-9...\n");
+            throw invalid_argument("not good - in input there is number between 5-9...\n");
         if(tmp%10==0)
-            throw invalid_argument("not good - number is 0...\n");
+            throw invalid_argument("not good - in input there is number 0...\n");
+        list[index_list] = tmp%10; // fill the list of our snowman
         tmp = tmp/10;
+        index_list--; 
         count ++;
     }
-    if(count != 8)
-        throw invalid_argument("not good\n");
-   
-    if(x == 11114411)
-        return "_===_\n(.,.)\n( : )\n( : )";
-
-    return " _===_\n\\(o.o)/\n ( : )\n (   )";
-    }
+    if(count > 8)
+        throw invalid_argument("not good - number of digits bigger then 8...\n");
+    if(count < 8)
+        throw invalid_argument("not good - number of digits smaller then 8...\n");
     
+    // cout<<"count is: "<< count<<endl;
+    
+    string snowman_out=""; // h n l r x y t b - order of snowman
+
+    snowman_out+=hat[list[0]-1];
+        if(list[4] == 2){
+            snowman_out+= "\\";
+        }
+        else{
+            snowman_out+=" ";
+        }
+
+    snowman_out+= left_eye[list[2]-1]+nose[list[1]-1]+right_eye[list[3]-1]; 
+
+        if(list[5] == 2){
+            snowman_out+= "/\n";
+        }
+        else{
+            snowman_out+=" \n";
+        }
+        if(list[4] != 2){
+            snowman_out+= left_arm[list[4]-1];
+        }
+        else{
+            snowman_out+=" ";
+        }
+
+    snowman_out+= torso[list[6]-1];
+
+        if(list[5] != 2){
+            snowman_out+= right_arm[list[5]-1]+"\n";
+        }
+        else{
+            snowman_out+=" \n";
+        }
+
+    snowman_out+=base[list[7]-1];
+            
+    // cout << "----------------------------" <<endl;
+    // cout << snowman_out <<endl;
+    // cout << "----------------------------" <<endl;
+
+    return snowman_out;
+    }
 }
 
 
